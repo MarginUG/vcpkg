965bc1b8d00611d5a4cd45b9953f8e4cf315d184
Marvin Schubert <marvin@margin.de>
2020-12-15 17:13:46 +0100

    upgrade openlimits-cpp

--- ports/openlimits-cpp/portfile.cmake
+++ ports/openlimits-cpp/portfile.cmake
@@ -11,7 +11,7 @@ vcpkg_add_to_path(${RUST_TOOLCHAIN})
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL https://github.com/MarginUG/openlimits-cpp.git
-    REF 8be1e3fc718c220d3a2fc0f5f67909762f09694e
+    REF 5fe6ae360998f0de0417cb33fdaf9484db40c678
 )
 # set(SOURCE_PATH "/home/marvin/sources/openlimits-cpp")
 # Use openlimit-cpp's own build process
